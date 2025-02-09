8cc2ccf11e33e75004a8f00fcf77105bd05cf675
James Arnold <oioi05@msn.com>
2025-02-08 19:41:21 -0700

    Add example modules

---
+++ src/module_a/script.py
@@ -0,0 +1,7 @@
+def main() -> str:
+    s = "Hello from module-a!"
+    return s
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/module_b/script.py
@@ -0,0 +1,7 @@
+def main() -> str:
+    s = "Hello from module-b!"
+    return s
+
+
+if __name__ == "__main__":
+    main()
